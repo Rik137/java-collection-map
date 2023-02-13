c6b70bee6b9e2d84b31cc82a57ccb9f89f4e3a31
Rik137 <ric19902002@icloud.com>
2023-02-13 12:39:25 +0300

    did home workd

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,7 +1,48 @@
 package practice;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        String phone = "";
+        String name = "";
+        while (true) {
+            System.out.println("Введите номер, имя или команду:");
+            String input = new Scanner(System.in).nextLine().strip();
+            if (input.equals("LIST")) {
+                System.out.println(phoneBook.getAllContacts());
+            } else {
+                String inputPhone = phoneBook.checkPhoneNumber(input);
+                String inputName = phoneBook.checkName(input);
 
+                if (!inputPhone.equals("-1")) {
+                    phone = input;
+                    if (phoneBook.getContactByPhone(inputPhone).equals("") && name.equals("")) {
+                        System.out.println("Такого номера нет в телефонной книге.\n" +
+                                "Введите имя абонента для номера " + "\"" + inputPhone + "\"" + " :");
+                    } else {
+                        System.out.println(phoneBook.getContactByPhone(phone));
+                    }
+                } else if (!inputName.equals("-1")) {
+                    name = input;
+                    if (phoneBook.getContactByName(input).equals(new TreeSet<>()) && phone.equals("")) {
+                        System.out.println("Такого имени в телефонной книге нет.\n" +
+                                "Введите номер телефона для абонента " + "\"" + inputName + "\"" + ": ");
+                    } else {
+                        System.out.println(phoneBook.getContactByName(name));
+                    }
+                } else {
+                    System.out.println("не корректный ввод!");
+                }
+                if (!name.equals("") && !phone.equals("")) {
+                    phoneBook.addContact(phone, name);
+                    System.out.println("контакт сохранен!");
+                    name = "";
+                    phone = "";
+                }
+            }
+        }
     }
 }
--- src/main/java/practice/PhoneBook.java
+++ src/main/java/practice/PhoneBook.java
@@ -1,40 +1,91 @@
 package practice;
 
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.TreeMap;
 
 public class PhoneBook {
+    Map<String, String> phoneBook = new TreeMap<>();
 
     public void addContact(String phone, String name) {
-        // проверьте корректность формата имени и телефона
-        // (рекомедуется написать отдельные методы для проверки является строка именем/телефоном)
-        // если такой номер уже есть в списке, то перезаписать имя абонента
+        if (!checkName(name).equals("-1") && !checkPhoneNumber(phone).equals("-1")) {
+            if (phoneBook.containsKey(phone)) {
+                phoneBook.replace(phone, phoneBook.get(phone), name);
+            } else if (phoneBook.containsValue(name)) {
+                getContactTwoNumbers(phone, name);
+            } else {
+                phoneBook.put(phone, name);
+            }
+        }
     }
 
     public String getContactByPhone(String phone) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найдены - вернуть пустую строку
-        return "";
+        if (phoneBook.containsKey(phone)) {
+            return phoneBook.get(phone) + " - " + phone;
+        } else {
+            return "";
+        }
     }
 
     public Set<String> getContactByName(String name) {
-        // формат одного контакта "Имя - Телефон"
-        // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
+        Set<String> setPhoneBook = new TreeSet<>();
+        String namePlusTel = "";
+        if (phoneBook.containsValue(name)) {
+            namePlusTel = namePlusTel.concat(name) + " - ";
+            for (String tel : phoneBook.keySet()) {
+                if (phoneBook.get(tel).equals(name)) {
+                    namePlusTel = namePlusTel.concat(tel) + ", ";
+                }
+            }
+            setPhoneBook.add(namePlusTel.substring(0, namePlusTel.length() - 2));
+            return setPhoneBook;
+        } else {
+            return new TreeSet<>();
+        }
     }
 
     public Set<String> getAllContacts() {
-        // формат одного контакта "Имя - Телефон"
-        // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+        Set<String> setPhoneBook = new TreeSet<>();
+        for (String key : phoneBook.keySet()) {
+            setPhoneBook.add(phoneBook.get(key) + " - " + key);
+        }
+        return setPhoneBook;
     }
 
-    // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
-    // это поможет вам найти все ключи (key) по значению (value)
-    /*
-        for (Map.Entry<String, String> entry : map.entrySet()){
-            String key = entry.getKey(); // получения ключа
-            String value = entry.getValue(); // получения ключа
+    public Set<String> getContactTwoNumbers(String phone, String name) {
+        TreeSet<String> result = new TreeSet<>();
+        String key = "";
+        String namePlusNumber = "";
+        if (phoneBook.containsValue(name)) {
+            for (String k : phoneBook.keySet()) {
+                if (phoneBook.get(k).equals(name)) {
+                    key = k;
+                    result.add(name + " - " + key + ", " + phone);
+                    namePlusNumber = key + ", " + phone;
+                    break;
+                }
+            }
         }
-    */
+        phoneBook.remove(key);
+        phoneBook.put(namePlusNumber, name);
+        return result;
+    }
+
+    public String checkPhoneNumber(String phone) {
+        String regex = "[^0-9]";
+        String regex1 = "[0-9]{11}";
+        String correctPhone = phone.replaceAll(regex, "");
+        if (!correctPhone.matches(regex1)) {
+            correctPhone = "-1";
+        }
+        return correctPhone;
+    }
+
+    public String checkName(String name) {
+        if (!name.matches("[А-Я][а-я]+")) {
+            name = "-1";
+        }
+        return name;
+    }
 }
